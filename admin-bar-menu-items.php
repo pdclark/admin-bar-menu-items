bec39537943ae85fbc22f6980001af823beba727
Paul Clark <github@pdclark.com>
2014-01-21 19:49:22 -0700

    Update admin-bar-menu-items.php
    Replace Brainstorm Media with 10up. Add link to Menus admin page in description.

--- admin-bar-menu-items.php
+++ admin-bar-menu-items.php
@@ -1,35 +1,14 @@
 <?php
-
 /*
 Plugin Name: Admin Bar Menu Items
-Plugin URI: https://github.com/brainstormmedia/admin-bar-menu-items
-Description: Add menu items to the Admin Bar using a menu called "Admin" in Appearance > Menus
+Plugin URI: https://github.com/10up/admin-bar-menu-items
+Description: Add menu items to the Admin Bar using a menu called "Admin" in <a href="nav-menus.php">Appearance > Menus</a>.
 Version: 1.0
-Author: Brainstorm Media
-Author URI: http://brainstormmedia.com 
+Author: Paul Clark, 10up
+Author URI: http://pdclark.com 
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
-
-/**
- * Copyright (c) 2012 Brainstorm Media. All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **********************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * **********************************************************************
- */
 
 // Add WP Menus to the Admin Menubar
 function storm_admin_bar_custom_menus() {
